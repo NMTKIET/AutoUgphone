dfb39a8203030131fe5bfb51f30e951e1ca1bf08
NMTKIET <theok0770@gmail.com>
2025-07-03 12:14:23 +0700

    Create tampermonkey.user.js

---
+++ tampermonkey.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         UGPhone Login Tool
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Ugphone Login
+// @author       NMTKIET
+// @require      https://github.com/KingNotNgu/localstorage/raw/refs/heads/main/ugphone.js
+// @match        https://www.ugphone.com/*
+// @match        https://*.ugphone.com/*
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @run-at       document-end
+// @icon         https://i.postimg.cc/nVD59Xmv/1.jpg
+// ==/UserScript==
